7519418ad8f697ba0dd9f719f4a84b9126b72dc2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 20:30:09 +0200

    Update Cart model

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -1,7 +1,22 @@
 const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
-
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    products: [
+        {
+            productId: {
+                type: mongoose.Types.ObjectId,
+                ref: "Product"
+            },
+            quantity: {
+                type: Number,
+                default: 1
+            }
+        }
+    ],
 });
 
 const cartModel = mongoose.model("Cart", cartSchema);
