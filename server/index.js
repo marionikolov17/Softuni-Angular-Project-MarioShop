5464ced9ade359c319e54bbfa21121c7cb96cfb7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 15:52:46 +0200

    Import and connect MongoDB database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 // Config imports
 const expressConfig = require("./config/expressConfig");
@@ -13,12 +14,21 @@ expressConfig(app);
 
 // Auth middleware
 
-
 // App router middleware
 app.use(router);
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+mongoose
+  .connect("mongodb://localhost:27017/", { dbName: "marioshop" })
+  .then(() => {
+    console.log("DB connected successfully!");
+    app.listen(PORT, () => {
+      console.log(
+        `Server is listening on port ${PORT} - http://localhost:${PORT}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
