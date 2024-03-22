3766e438f787336bcfb23483398ad3babaf51cc4
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 20:43:50 +0200

    Create products service basic functions

---
+++ server/services/productsService.js
@@ -0,0 +1,19 @@
+const productsModel = require("./../models/Product");
+
+const getProducts = async () => productsModel.find();
+
+const getProduct = async (id) => productsModel.findById(id);
+
+const createProduct = async (data) => productsModel.create(data);
+
+const updateProduct = async (id, data) => productsModel.findByIdAndUpdate(id, data);
+
+const deleteProduct = async (id) => productsModel.findByIdAndDelete(id);
+
+module.exports = {
+    getProducts,
+    getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
+}
