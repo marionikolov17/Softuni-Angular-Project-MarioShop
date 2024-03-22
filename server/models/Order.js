385ae86da13e0f6e1361441076bbabcafbfe9660
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 20:28:39 +0200

    Update Order model

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,7 +1,34 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    products: [
+        {
+            productId: {
+                type: mongoose.Types.ObjectId,
+                ref: "Product"
+            },
+            quantity: {
+                type: Number,
+                default: 1
+            }
+        }
+    ],
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
 });
 
 const orderModel = mongoose.model("Order", orderSchema);
