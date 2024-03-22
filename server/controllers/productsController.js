6c914a833335944b0ff84098288755ed77f9727d
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 20:49:31 +0200

    Implement create product

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -1,9 +1,17 @@
 const { isAuth, isAdmin } = require("../middlewares/authMiddleware");
+const productsService = require("./../services/productsService");
+const { getErrorMessage } = require("./../utils/errorUtil");
 
 const router = require("express").Router();
 
-router.post("/", isAuth, isAdmin, (req, res) => {
-    res.json({ message: "OK" });
-})
+router.post("/", isAuth, isAdmin, async (req, res) => {
+    try {
+        await productsService.createProduct(req.body);
+
+        res.status(201).json({ status: "success", data: { message: "Product succesfully created!" } })
+    } catch (err) {
+        res.status(400).json({ status: "fail", data: { message: getErrorMessage(err) } });
+    }
+});
 
 module.exports = router;
