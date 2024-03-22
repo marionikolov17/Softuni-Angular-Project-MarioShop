7c4dfb81997bfff6a449fc95bc8f7fe998b4bbf1
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 20:52:15 +0200

    Implement get all products

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -4,14 +4,34 @@ const { getErrorMessage } = require("./../utils/errorUtil");
 
 const router = require("express").Router();
 
-router.post("/", isAuth, isAdmin, async (req, res) => {
-    try {
-        await productsService.createProduct(req.body);
+/* Get all products */
+router.get("/", async (req, res) => {
+  const products = await productsService.getProducts();
 
-        res.status(201).json({ status: "success", data: { message: "Product succesfully created!" } })
-    } catch (err) {
-        res.status(400).json({ status: "fail", data: { message: getErrorMessage(err) } });
-    }
+  res.status(200).json({
+    status: "success",
+    data: {
+      products,
+    },
+  });
+});
+
+/* Create product */
+router.post("/", isAuth, isAdmin, async (req, res) => {
+  try {
+    await productsService.createProduct(req.body);
+
+    res
+      .status(201)
+      .json({
+        status: "success",
+        data: { message: "Product succesfully created!" },
+      });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ status: "fail", data: { message: getErrorMessage(err) } });
+  }
 });
 
 module.exports = router;
