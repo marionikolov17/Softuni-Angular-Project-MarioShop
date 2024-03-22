c17e9142ae4f2afb682a238b0f64d68e26524799
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 20:54:39 +0200

    Implement get single product

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -21,16 +21,30 @@ router.post("/", isAuth, isAdmin, async (req, res) => {
   try {
     await productsService.createProduct(req.body);
 
-    res
-      .status(201)
-      .json({
-        status: "success",
-        data: { message: "Product succesfully created!" },
-      });
+    res.status(201).json({
+      status: "success",
+      data: { message: "Product succesfully created!" },
+    });
   } catch (err) {
     res
       .status(400)
       .json({ status: "fail", data: { message: getErrorMessage(err) } });
+  }
+});
+
+/* Get One Product */
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await productsService.getProduct(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({ status: "fail", data: { message: "Product not found!" } });
   }
 });
 
