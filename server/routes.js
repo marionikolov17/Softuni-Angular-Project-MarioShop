128b92f2164e2338b4df05221f5d580be23651e8
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 15:58:11 +0200

    Import ProductsController in app router

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 
 // Controllers import
 const authController = require("./controllers/authController");
+const productsController = require("./controllers/productsController");
 
 router.use(authController);
+router.use(productsController);
 
 module.exports = router;
