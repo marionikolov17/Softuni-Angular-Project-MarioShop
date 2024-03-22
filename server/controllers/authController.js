0e3eb5c6da469202fe5416de56ad99e581dd8f36
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 16:26:27 +0200

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,43 @@
 const router = require("express").Router();
 
+const authService = require("./../services/authService");
+
+const { isAuth } = require("./../middlewares/authMiddleware");
+
+const { getErrorMessage } = require("./../utils/errorUtil");
+
+router.post("/register", async (req, res) => {
+  try {
+    const token = await authService.registerUser(req.body);
+
+    res.cookie("auth", token);
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      data: {
+        errorMessage: getErrorMessage(err),
+      },
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const token = await authService.loginUser(req.body);
+
+    res.cookie("auth", token);
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      data: {
+        errorMessage: getErrorMessage(err),
+      },
+    });
+  }
+});
+
+router.get("/logout", isAuth, (req, res) => {
+  res.clearCookie("auth");
+});
+
 module.exports = router;
