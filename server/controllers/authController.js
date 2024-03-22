202c9a874adea58b27e193579c76f7032d018bd2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 16:34:58 +0200

    Update login logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,6 +33,12 @@ router.post("/login", async (req, res) => {
     const token = await authService.loginUser(req.body);
 
     res.cookie("auth", token);
+    res.status(200).json({
+        status: "success",
+        data: {
+            token
+        }
+    });
   } catch (err) {
     res.status(400).json({
       status: "fail",
