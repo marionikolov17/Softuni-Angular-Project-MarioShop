73d07ccd575816048c479e7888a8fb5db725722a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 16:10:38 +0200

    Update isAuth guard middleware

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -2,34 +2,36 @@ const jwt = require("./../lib/jwt");
 const SECRET = require("./../config/secret");
 
 const authMiddleware = async (req, res, next) => {
-    const token = req.cookies["auth"];
+  const token = req.cookies["auth"];
 
-    if (!token) {
-        return next();
-    }
+  if (!token) {
+    return next();
+  }
 
-    try {
-        const decoded = await jwt.verify(token, SECRET);
+  try {
+    const decoded = await jwt.verify(token, SECRET);
 
-        req.user = decoded;
+    req.user = decoded;
 
-        res.locals.isAuthenticated = true;
-
-        next();
-    } catch (err) {
-        next();
-    }
-}
-
-const isAuth = (req, res, next) => {
-    if (!req.user) {
-        return res.redirect("/auth/login");
-    }
+    res.locals.isAuthenticated = true;
 
     next();
-}
+  } catch (err) {
+    next();
+  }
+};
+
+const isAuth = (req, res, next) => {
+  if (!req.user) {
+    return res
+      .status(401)
+      .json({ status: "fail", data: { message: "You must login!" } });
+  }
+
+  next();
+};
 
 module.exports = {
-    authMiddleware,
-    isAuth,
-}
+  authMiddleware,
+  isAuth,
+};
