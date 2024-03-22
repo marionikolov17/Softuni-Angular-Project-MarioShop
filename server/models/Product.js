3ddc04b90ec8eafcc56c2d54447d58a0a9cf16f6
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 20:25:53 +0200

    Update Product Model

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,7 +1,29 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageSrc: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ["computers", "laptops"]
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    }
 });
 
 const productModel = mongoose.model("Product", productSchema);
