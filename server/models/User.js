40055d795a5dfe2e547331c061e006adac275c4f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 16:16:10 +0200

    Create User Model

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, {
+    virtuals: {
+        rePassword: {}
+    }
+});
+
+userSchema.pre("save", async function (next) {
+    if (this.password !== this.rePassword) {
+        throw new Error("Passwords must match!");
+    }
+
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+
+    this.password = hashedPassword;
+
+    next();
+});
+
+const userModel = mongoose.model("User", userSchema);
+module.exports = userModel;
