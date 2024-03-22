0b4d7baa2db9468fab90347bdd8b6ac25094daba
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-22 15:56:50 +0200

    Import AuthController in app router

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
 
+// Controllers import
+const authController = require("./controllers/authController");
+
+router.use(authController);
+
 module.exports = router;
