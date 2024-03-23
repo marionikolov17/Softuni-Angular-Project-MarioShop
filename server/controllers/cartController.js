8ea0bf6bdf9cfe22677d331d1bcbbd65a16fcc25
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-23 13:35:04 +0200

    Update cartController.js

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,3 +1,20 @@
 const router = require("express").Router();
 
+const cartService = require("./../services/cartService");
+
+const { isAuth, isAdmin } = require("../middlewares/authMiddleware");
+const { getErrorMessage } = require("./../utils/errorUtil");
+
+router.param("id", async (req, res, next) => {
+  try {
+    const cart = await cartService.getCart(req.params.id);
+
+    next();
+  } catch (err) {
+    res
+      .status(404)
+      .json({ status: "fail", data: { message: "Order not found!" } });
+  }
+});
+
 module.exports = router;
