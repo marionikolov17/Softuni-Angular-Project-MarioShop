4307eca0bc85eb8c6608020547bb34e0079524dd
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-23 13:01:00 +0200

    Implement get all orders

--- server/controllers/ordersController.js
+++ server/controllers/ordersController.js
@@ -1,3 +1,27 @@
 const router = require("express").Router();
 
+const ordersService = require("./../services/ordersService");
+
+const { isAuth } = require("../middlewares/authMiddleware");
+const { getErrorMessage } = require("./../utils/errorUtil");
+
+router.param("id", async (req, res, next) => {
+  try {
+    const order = await ordersService.getOrder(req.params.id);
+
+    next();
+  } catch (err) {
+    res
+      .status(404)
+      .json({ status: "fail", data: { message: "Order not found!" } });
+  }
+});
+
+/* Get all orders */
+router.get("/", async (req, res) => {
+  const orders = await ordersService.getOrders();
+
+  res.status(200).json({ status: "success", data: { orders } });
+});
+
 module.exports = router;
