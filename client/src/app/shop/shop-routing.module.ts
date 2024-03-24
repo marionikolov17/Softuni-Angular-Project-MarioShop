5232d89627ecbd0aa8bf9dd7224fddd4886353cd
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-24 19:28:03 +0200

    Implement routes for user auth in shop

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -4,12 +4,18 @@ import { ShopComponent } from './shop.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { CartComponent } from './cart/cart.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: "shop", component: ShopComponent, children: [
     { path: "", pathMatch: "full", component: HomeComponent },
     { path: "cart", component: CartComponent },
     { path: ":id", component: ProductComponent }
+  ] },
+  { path: "auth", component: ShopComponent, children: [
+    { path: "register", component: RegisterComponent },
+    { path: "login", component: LoginComponent }
   ] }
 ];
 
