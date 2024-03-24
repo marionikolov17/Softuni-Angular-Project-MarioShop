69560225a989a5c1a2de9e67d932feec1549b323
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-24 19:00:52 +0200

    Make route for cart page

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ShopComponent } from './shop.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: "shop", component: ShopComponent, children: [
     { path: "", pathMatch: "full", component: HomeComponent },
+    { path: "cart", component: CartComponent },
     { path: ":id", component: ProductComponent }
   ] }
 ];
