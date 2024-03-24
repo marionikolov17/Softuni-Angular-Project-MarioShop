00e2f6538001a3b43afa508532c7ed3415eb1957
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-24 18:53:17 +0200

    Make path for product page

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShopComponent } from './shop.component';
 import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   { path: "shop", component: ShopComponent, children: [
-    { path: "", pathMatch: "full", component: HomeComponent }
+    { path: "", pathMatch: "full", component: HomeComponent },
+    { path: ":id", component: ProductComponent }
   ] }
 ];
 
