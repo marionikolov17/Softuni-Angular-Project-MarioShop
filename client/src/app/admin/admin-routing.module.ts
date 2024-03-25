af55377bc54fb9be19474cc8f819af9b085a12bd
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-25 20:58:25 +0200

    Make router for Admin Page

--- client/src/app/admin/admin-routing.module.ts
+++ client/src/app/admin/admin-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 const routes: Routes = [
   { path: "admin", children: [
+    { path: "", pathMatch: "full", component: AdminPageComponent },
     { path: "login", component: AdminLoginComponent }
   ] }
 ];
