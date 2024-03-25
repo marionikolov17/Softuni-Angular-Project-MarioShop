32969468bfff89ad5e8b9b510ce166b8ee4acd26
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-25 20:25:41 +0200

    Create registerForm group

--- client/src/app/shop/auth/register/register.component.ts
+++ client/src/app/shop/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm = this.fb.group({
+    username: ['', Validators.required, Validators.minLength(4)],
+    email: ['', Validators.required, Validators.email],
+    passGroup: this.fb.group({
+      password: ['', Validators.required, Validators.minLength(4)],
+      rePassword: ['', Validators.required]
+    })
+  });
 
+  constructor(private fb: FormBuilder) {}
 }
