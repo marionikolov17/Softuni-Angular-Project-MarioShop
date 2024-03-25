0de8be6acf9a73324629957c719ae4a1c425526b
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-25 20:57:48 +0200

    Create admin page component

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin-routing.module';
 import { CoreModule } from '../core/core.module';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 
 
 @NgModule({
   declarations: [
-    AdminLoginComponent
+    AdminLoginComponent,
+    AdminPageComponent
   ],
   imports: [
     CommonModule,
