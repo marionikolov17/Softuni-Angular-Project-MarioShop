1805be64beb926abac4237c4165267311970d219
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-25 21:04:00 +0200

    Import FontAwesomeModule in admin

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin-routing.module';
 import { CoreModule } from '../core/core.module';
 import { AdminPageComponent } from './admin-page/admin-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
@@ -17,7 +18,8 @@ import { AdminPageComponent } from './admin-page/admin-page.component';
     CommonModule,
     AdminRoutingModule,
     ReactiveFormsModule,
-    CoreModule
+    CoreModule,
+    FontAwesomeModule
   ]
 })
 export class AdminModule { }
