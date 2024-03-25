6c035e61815b81ecb7afe47aaa2e1a96c6f2feb1
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-25 20:08:13 +0200

    Import Reactive Forms in shop auth

--- client/src/app/shop/auth/auth.module.ts
+++ client/src/app/shop/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -15,7 +16,8 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
