04af9196b2220da0a7f3a280cec1117e99e7181d
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-26 16:39:17 +0200

    Dynamic loading in product page functionalitty

--- client/src/app/shop/product/product.component.ts
+++ client/src/app/shop/product/product.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ShopService } from '../shop.service';
+import { Product } from 'src/app/types/product';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  product: Product | undefined;
 
+  constructor(private activeRoute: ActivatedRoute, private shopService: ShopService) {}
+
+  ngOnInit(): void {
+      this.activeRoute.params.subscribe((data: any) => {
+        this.shopService.getProduct(data.id).subscribe((response: any) => {
+          console.log(response);
+          this.product = response.data.product;
+        });
+      });
+  }
 }
