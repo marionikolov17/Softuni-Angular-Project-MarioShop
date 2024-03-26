cd90d02c6bd7c6fd8c3050c45dc03ff6f8881618
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-26 16:34:18 +0200

    Create getProduct method in shop service

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -11,4 +11,8 @@ export class ShopService {
   getProducts() {
     return this.httpClient.get("/api/products");
   }
+
+  getProduct(id: string) {
+    return this.httpClient.get("/api/products/" + id);
+  }
 }
