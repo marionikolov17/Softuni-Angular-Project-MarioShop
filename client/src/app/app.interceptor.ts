9ac2a2833d5b46b958924c381369c52fb9e7a95f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-26 16:11:38 +0200

    Update app.interceptor.ts

--- client/src/app/app.interceptor.ts
+++ client/src/app/app.interceptor.ts
@@ -1,18 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Provider } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HTTP_INTERCEPTORS
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+const apiUrl = "http://localhost:3000"
 
 @Injectable()
-export class AppInterceptor implements HttpInterceptor {
+class AppInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (request.url.startsWith("/api")) {
+      request = request.clone({
+        url: request.url.replace("/api", apiUrl),
+        withCredentials: true
+      });
+    }
+    
     return next.handle(request);
   }
 }
+
+export const appInterceptorProvider: Provider = {
+  useClass: AppInterceptor,
+  multi: true,
+  provide: HTTP_INTERCEPTORS,
+};
