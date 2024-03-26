147a0fd7805227bb839d8445613cf7562618e910
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-26 16:04:55 +0200

    Update product.ts

---
+++ client/src/app/types/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  _id: string;
+  title: string;
+  description: string;
+  imageSrc: string;
+  category: string;
+  price: number;
+  createdAt: string;
+  __v: number
+}
