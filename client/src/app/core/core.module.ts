2c8b0bbd0b34603220a6d8ccf4d745105714d651
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-26 17:18:12 +0200

    Create Error Component

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,12 +4,14 @@ import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ErrorComponent
   ],
   exports: [
     HeaderComponent,
