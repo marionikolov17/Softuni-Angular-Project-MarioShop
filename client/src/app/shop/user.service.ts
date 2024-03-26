90a30304bc9f02f25035b24f819a5fe45f5a8025
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-26 17:01:07 +0200

    Update user.service.ts

--- client/src/app/shop/user.service.ts
+++ client/src/app/shop/user.service.ts
@@ -1,10 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
+import { User } from '../types/user';
+import { Token } from '@angular/compiler';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private user$$ = new BehaviorSubject<User | undefined>(undefined);
+  private user$ = this.user$$.asObservable();
 
-  constructor(private httpClient: HttpClient) { }
+  user: User | undefined;
+
+  //userSubscription: Subscription;
+
+  constructor(private httpClient: HttpClient) {}
+
+  register() {}
+
+  login(email: string, password: string) {
+    return this.httpClient
+      .post('/api/auth/login', { email, password })
+      .pipe(tap((response) => console.log(response)));
+  }
+
+  logout() {}
 }
