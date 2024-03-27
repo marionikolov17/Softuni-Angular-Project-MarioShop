a668324a8c07853e7fe6e9565a1e0c52ce0d72bc
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-27 21:24:50 +0200

    Implement add to cart functionalitty

--- client/src/app/shop/product/product.component.ts
+++ client/src/app/shop/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ShopService } from '../shop.service';
 import { Product } from 'src/app/types/product';
 
@@ -11,7 +11,7 @@ import { Product } from 'src/app/types/product';
 export class ProductComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private activeRoute: ActivatedRoute, private shopService: ShopService) {}
+  constructor(private activeRoute: ActivatedRoute, private shopService: ShopService, private router: Router) {}
 
   ngOnInit(): void {
       this.activeRoute.params.subscribe((data: any) => {
@@ -20,5 +20,15 @@ export class ProductComponent implements OnInit {
           this.product = response.data.product;
         });
       });
+  }
+
+  addToCart() {
+    this.shopService.getCart().subscribe((response: any) => {
+      const products = response.data.cart.products;
+      products.push({ productId: this.product?._id });
+      this.shopService.updateCart(products).subscribe(() => {
+        this.router.navigate(['/shop/cart']);
+      })
+    })
   }
 }
