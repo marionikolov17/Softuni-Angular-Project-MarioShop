21c2236041ca6e2e60510f57f39830cd1c40100c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-28 15:00:28 +0200

    Implement filter and sort home functionalitty

--- client/src/app/shop/home/home.component.ts
+++ client/src/app/shop/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ShopService } from '../shop.service';
 import { Product } from 'src/app/types/product';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -15,12 +15,17 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private shopService: ShopService,
-    private activeRoute: ActivatedRoute
+    private activeRoute: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    console.log(this.activeRoute.snapshot.queryParams);
-    this.shopService.getProducts(this.activeRoute.snapshot.queryParams).subscribe((response: any) => {
+    //console.log(this.activeRoute.snapshot.queryParams);
+    this.loadProducts(this.activeRoute.snapshot.queryParams);
+  }
+
+  loadProducts(queryParams: any) {
+    this.shopService.getProducts(queryParams).subscribe((response: any) => {
       console.log(response.data);
       this.products = response.data.products;
     });
@@ -32,5 +37,29 @@ export class HomeComponent implements OnInit {
 
   toggleSort() {
     this.isSortOpened = !this.isSortOpened;
+  }
+
+  changeCategory(category: string) {
+    const newParams = {...this.activeRoute.snapshot.queryParams, category: category};
+    this.router.navigate(['/shop'], { queryParams: newParams });
+    this.loadProducts(newParams);
+  }
+
+  changeMinPrice(event: any) {
+    const newParams = {...this.activeRoute.snapshot.queryParams, minPrice: event.target.value};
+    this.router.navigate(['/shop'], { queryParams: newParams });
+    this.loadProducts(newParams);
+  }
+
+  changeMaxPrice(event: any) {
+    const newParams = {...this.activeRoute.snapshot.queryParams, maxPrice: event.target.value};
+    this.router.navigate(['/shop'], { queryParams: newParams });
+    this.loadProducts(newParams);
+  }
+
+  changeSortBy(sortBy: string) {
+    const newParams = {...this.activeRoute.snapshot.queryParams, sortBy};
+    this.router.navigate(['/shop'], { queryParams: newParams });
+    this.loadProducts(newParams);
   }
 }
