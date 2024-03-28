9d770dbb4c084cd0be3fc8d90c298267723b4bd4
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-28 20:58:01 +0200

    Update product add to cart

--- client/src/app/shop/product/product.component.ts
+++ client/src/app/shop/product/product.component.ts
@@ -2,33 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ShopService } from '../shop.service';
 import { Product } from 'src/app/types/product';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private activeRoute: ActivatedRoute, private shopService: ShopService, private router: Router) {}
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private shopService: ShopService,
+    private router: Router,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
-      this.activeRoute.params.subscribe((data: any) => {
-        this.shopService.getProduct(data.id).subscribe((response: any) => {
-          console.log(response);
-          this.product = response.data.product;
-        });
+    this.activeRoute.params.subscribe((data: any) => {
+      this.shopService.getProduct(data.id).subscribe((response: any) => {
+        console.log(response);
+        this.product = response.data.product;
       });
+    });
   }
 
   addToCart() {
-    this.shopService.getCart().subscribe((response: any) => {
-      const products = response.data.cart.products;
-      products.push({ productId: this.product?._id });
-      this.shopService.updateCart(products).subscribe(() => {
-        this.router.navigate(['/shop/cart']);
-      })
-    })
+    if (!this.userService.isLoggedIn) {
+      this.router.navigate(['/auth/login']);
+    } else {
+      this.shopService.getCart().subscribe((response: any) => {
+        const products = response.data.cart.products;
+        products.push({ productId: this.product?._id });
+        this.shopService.updateCart(products).subscribe(() => {
+          this.router.navigate(['/shop/cart']);
+        });
+      });
+    }
   }
 }
