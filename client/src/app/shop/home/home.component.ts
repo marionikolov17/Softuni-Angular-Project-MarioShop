12e5ce566eaac458807213b6455d65a5cdf5bd68
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-31 14:53:57 +0300

    Update home.component.ts

--- client/src/app/shop/home/home.component.ts
+++ client/src/app/shop/home/home.component.ts
@@ -79,7 +79,4 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/shop'], { queryParams: newParams });
     this.loadProducts(newParams);
   }
-
-  /* Slider functionalitty */
-  onRangeInput(event: any) {}
 }
