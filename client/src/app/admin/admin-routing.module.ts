768e6314057ed6c50f0ab0312bd2fdba9b046777
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-31 14:00:19 +0300

    Protect admin page

--- client/src/app/admin/admin-routing.module.ts
+++ client/src/app/admin/admin-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminPageComponent } from './admin-page/admin-page.component';
+import { AdminAuthActivate } from '../guards/admin-auth.activate';
 
 const routes: Routes = [
   { path: "admin", children: [
-    { path: "", pathMatch: "full", component: AdminPageComponent },
+    { path: "", pathMatch: "full", component: AdminPageComponent, canActivate: [AdminAuthActivate] },
     { path: "login", component: AdminLoginComponent }
   ] }
 ];
