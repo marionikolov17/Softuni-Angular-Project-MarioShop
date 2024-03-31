520d42bb6836770088ba8a26951794307cca56a9
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-03-31 13:39:30 +0300

    create checking for admin

--- client/src/app/admin/admin-auth.service.ts
+++ client/src/app/admin/admin-auth.service.ts
@@ -1,10 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../types/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminAuthService {
+  user: User | undefined
 
   constructor(private httpClient: HttpClient) { }
+
+  get isAdminLoggedIn() {
+    return !!this.user && this.user?.isAdmin;
+  }
 }
