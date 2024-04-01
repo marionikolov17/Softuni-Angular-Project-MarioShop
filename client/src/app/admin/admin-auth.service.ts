48d79fdf8675116757bb53ff91ec2dfb3fd52ae4
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-04-01 13:46:03 +0300

    Update admin-auth.service.ts

--- client/src/app/admin/admin-auth.service.ts
+++ client/src/app/admin/admin-auth.service.ts
@@ -1,30 +1,56 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { User } from '../types/user';
-import { tap } from 'rxjs';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AdminAuthService {
+export class AdminAuthService implements OnDestroy {
+  private user$$ = new BehaviorSubject<User | undefined>(undefined);
+  private user$ = this.user$$.asObservable();
+
   user: User | undefined;
 
-  constructor(private httpClient: HttpClient) {}
+  userSubscription: Subscription;
+
+  constructor(private httpClient: HttpClient) {
+    this.userSubscription = this.user$.subscribe((user) => {
+      this.user = user;
+    });
+
+    this.getCurrentUser();
+  }
 
   get isAdminLoggedIn() {
     return !!this.user && this.user?.isAdmin;
+  }
+
+  getCurrentUser() {
+    this.httpClient.get('/api/auth/admin').subscribe({
+      next: (response: any) => {
+        this.user$$.next(response.data.user);
+      },
+      error: () => {
+        return;
+      }
+    });
   }
 
   login(email: string, password: string) {
     return this.httpClient
       .post('/api/auth/admin-login', { email, password })
       .pipe(tap((response: any) => {
-        this.user = response.data.user;
+        this.user$$.next(response.data.user);
       }));
   }
 
   logout() {
     this.user = undefined;
     return this.httpClient.get("/api/auth/admin-logout").subscribe();
+  }
+
+  ngOnDestroy(): void {
+      this.userSubscription.unsubscribe();
   }
 }
