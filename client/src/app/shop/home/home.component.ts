31cd3ab8b0dccb00cae2b12749399493721cbbfa
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-04-01 13:55:54 +0300

    Update home.component.ts

--- client/src/app/shop/home/home.component.ts
+++ client/src/app/shop/home/home.component.ts
@@ -32,7 +32,7 @@ export class HomeComponent implements OnInit {
 
   loadProducts(queryParams: any) {
     this.shopService.getProducts(queryParams).subscribe((response: any) => {
-      console.log(response.data);
+      //console.log(response.data);
       this.products = response.data.products;
     });
   }
